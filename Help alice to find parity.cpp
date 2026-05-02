ad24a4bf4012a83504ba2abad44a7b46ab535350
chiranshi04 <chiranshiyadav919@gmail.com>
2026-05-02 17:19:08 +0530

    Create Help alice to find parity.cpp

---
+++ Help alice to find parity.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+long long f(long long n)
+ {
+    if (n % 4 == 0)
+	 return n;
+    if (n % 4 == 1) 
+	return 1;
+    if (n % 4 == 2) 
+	return n + 1;
+    return 0;
+}
+
+int main() 
+{
+    long long L, R;
+    cin >> L >> R;
+
+    long long x = f(R) ^ f(L - 1);
+
+    if (x % 2 == 0)
+        cout << "even";
+    else
+        cout << "odd";
+
+    return 0;
+}
